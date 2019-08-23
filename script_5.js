b5866742f25fa1991af803eeae0bd97296894341
tsiry <YOUR@EMAIL.com>
2019-08-23 08:58:35 +0300

    update

--- script_5.js
+++ script_5.js
@@ -1,36 +1,73 @@
+
 const books = [
-    { title: 'Gatsby le magnifique', id: 133712, rented: 39 },
-    { title: 'A la recherche du temps,perdu', id: 237634, rented: 28 },
-    { title: 'Orgueil & Préjugés', id: 873495, rented: 67 },
-    { title: 'Les frères Karamazov', id: 450911, rented: 55 },
-    { title: 'Dans les forêts de Sibérie', id: 8376365, rented: 15 },
-    { title: 'Pourquoi j\'ai mangé mon père', id: 450911, rented: 45 },
-    { title: 'Et on tuera tous les affreux', id: 67565, rented: 36 },
-    { title: 'Le meilleur des mondes', id: 88847, rented: 58 },
-    { title: 'La disparition', id: 364445, rented: 33 },
-    { title: 'La lune seule le sait', id: 63541, rented: 43 },
-    { title: 'Voyage au centre de la Terre', id: 4656388, rented: 38 },
-    { title: 'Guerre et Paix', id: 748147, rented: 19 }
-  ];
+  { title: 'Gatsby le magnifique', id: 133712, rented: 39 },
+  { title: 'A la recherche du temps,perdu', id: 237634, rented: 28 },
+  { title: 'Orgueil & Préjugés', id: 873495, rented: 67 },
+  { title: 'Les frères Karamazov', id: 450911, rented: 55 },
+  { title: 'Dans les forêts de Sibérie', id: 8376365, rented: 15 },
+  { title: 'Pourquoi j\'ai mangé mon père', id: 450911, rented: 45 },
+  { title: 'Et on tuera tous les affreux', id: 67565, rented: 36 },
+  { title: 'Le meilleur des mondes', id: 88847, rented: 58 },
+  { title: 'La disparition', id: 364445, rented: 33 },
+  { title: 'La lune seule le sait', id: 63541, rented: 43 },
+  { title: 'Voyage au centre de la Terre', id: 4656388, rented: 38 },
+  { title: 'Guerre et Paix', id: 748147, rented: 19 }
+];
+
+//Est-ce que tous les livres ont été au moins empruntés une fois ?
 
 
-  //Est-ce que tous les livres ont été au moins empruntés une fois ?
+function rentedOne() {
+  console.log("Est-ce que tous les livres ont été au moins empruntés une fois ?");
+  for(let index in books) {
 
-  //Quel est livre le plus emprunté ?
-  tab = []
- 
-  for (var i in books){
-    let obs = books[i].rented;
-    tab.push( obs );
+  if (books[index].rented>=1){
+    var reponse="OUI";
+  }
+  else {
+     reponse = "NON";
+  break; 
+  }
 }
-    tab.sort()
-    console.log('le livre le plus emprinter est :' + tab[tab.length-1] )
-  
-    //Quel est le livre le moins emprunté ?
-    console.log('le livre le moins emprinter est :' + tab[0])
-  
-    //Trouve le livre avec l'ID: 873495 ;
+ console.log(`>>>>>>>> ${reponse} <<<<<<<`);
+}
 
 
-  //Supprime le livre avec l'ID: 133712 ;
-  //Trie les livres par ordre alphabétique (sans celui avec l'ID 133712 car il est supprimé).
+
+function maxRent(){
+  console.log("Quel est livre le plus emprunté ?");
+  var rent = [];
+  for(var index in books){
+    var a =books[index].rented;
+    rent.push(a);
+  }
+  let maxi = Math.max(...rent);
+
+  for(var indexe in books){
+  if (books[indexe].rented === maxi) { 
+    console.log(`>>>>>>>>>>>   ${books[indexe].title}  <<<<<<<<<<`);
+  }
+ } 
+}
+
+function findBookWithAnID(id){
+
+  for(var index in books){
+    var a =books[index].id;
+    if (a === id) {
+      console.log(`Le qui a pour id = ${id} est ${books[index].title}`);
+    }
+  }
+}
+
+
+
+function deleteBook(id){
+  for(var index in books){
+  var a = books[index].id;
+    if (a === id) {
+      books.splice(indexOf(books.findIndex(ligne=>ligne.id=id),1));
+    }
+  
+}
+}
